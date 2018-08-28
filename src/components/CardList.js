c0ab68fba32a5dbac0a776aeb5a08f135ae3091e
Ariel Bobadilla <arielbdilla@gmail.com>
2018-08-27 20:10:38 -0400

    Changed CardList to be a functional component

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,29 +1,26 @@
 import React from 'react'
 import QuestionCard from './QuestionCard'
 
-class CardList extends React.Component {
-  render() {
-    const { questionIds, currentQuestionId, questionsById } = this.props
-    const historyIds = questionIds
-      .filter(id => id != currentQuestionId)
-      .reverse()
+const CardList = ({ questionIds, currentQuestionId, questionsById }) => {
+  const historyIds = questionIds
+    .filter(id => id != currentQuestionId)
+    .reverse()
 
-    return (
-      <div className='question-history'>
-        <div className='question-history__list'>
-          {
-            <ul>
-              {historyIds.map(id => (
-                <li className='question-history__item' key={id}>
-                  <QuestionCard {...questionsById[id]} />
-                </li>
-              ))}
-            </ul>
-          }
-        </div>
+  return (
+    <div className='question-history'>
+      <div className='question-history__list'>
+        {
+          <ul>
+            {historyIds.map(id => (
+              <li className='question-history__item' key={id}>
+                <QuestionCard {...questionsById[id]} />
+              </li>
+            ))}
+          </ul>
+        }
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default CardList
